9cf8b8c818ccbeaf7c0de0e387990ffa839087f2
AendruDavis <masindeandrewdavis@gmail.com>
2023-09-27 11:34:52 +0300

    this is the first commit

---
+++ PROJECTS/cartwheel/resources/views/footer.blade.php
@@ -0,0 +1,35 @@
+
+<style>
+    /* Add a black background color to the top navigation */
+.topnav {
+    margin-top:30px;
+  background-color: #333;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+  border-bottom: 3px solid transparent;
+}
+
+.topnav a:hover {
+  border-bottom: 3px solid red;
+}
+
+.topnav a.active {
+  border-bottom: 3px solid red;
+}
+</style>
+
+<div class="topnav">
+  <a href="#home" class="active">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+</div>
